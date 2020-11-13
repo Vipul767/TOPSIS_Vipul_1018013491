87d7b83d463b0218d22c99378f0a29f4e67b3447
Vipul767 <62534176+Vipul767@users.noreply.github.com>
2020-11-13 19:09:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=['TOPSIS_Vipul_101803491'],
     author_email='vgoel_be18@thapar.edu',
     description='TOPSIS implementation',
-    long_description=readme(),
+    long_description='TOPSIS_Vipul_101803491 is an implementation of Technique of Order Preference Similarity to the Ideal Solution(TOPSIS). It will help you to place your data acc to preference that is by looking at the data and performing some Maths it will tell you the Preference in which you will be most benefitted. If you want to run the code from command line use the following format: python topsis.py <InputDataFile> <Weights> <Impacts> <ResultFileName> Example: python topsis.py inputfile.csv “1,1,1,2” “+,+,-,+” result.csv',
     long_description_content_type="text/markdown",
     url="https://github.com/Vipul767/TOPSIS_Vipul_1018013491",
     download_url = "https://github.com/Vipul767/TOPSIS_Vipul_1018013491/archive/v_07.tar.gz",  
